6fec5245b43f311f7a8e5c633a83e012187b1e93
KazuoMurakami <otaviokazuo12@hotmail.com>
2024-04-03 23:49:16 -0300

    First Commit

---
+++ src/routes/create-events.ts
@@ -0,0 +1,51 @@
+import { ZodTypeProvider } from "fastify-type-provider-zod"
+import {z} from "zod"
+import { generateSlug } from "../utils/generate-slug"
+import { prisma } from "../lib/prisma"
+import { FastifyInstance } from "fastify"
+
+export async function createEvent(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .post("/events",{
+    schema: {
+        body: z.object({
+        title: z.string().min(4),
+        details: z.string().nullable(),
+        maximumAttendees: z.number().int().positive().nullable(),
+    }),
+    response: {
+        201: z.object({
+        eventId: z.string().uuid(), // espera que seja uma string com o UUID
+        })
+    }
+    }
+    }, async(request, reply) => {
+
+    const data = request.body
+    const slug = generateSlug(data.title)
+
+    const EventWithSameSlug = await prisma.event.findUnique({
+        where: {
+        slug,
+        }
+    })
+
+    if(EventWithSameSlug !== null) {
+        throw new Error("Outro evento com esse titulo já existe!")
+    }
+
+    const event = await prisma.event.create({
+        data: {
+        title: data.title,
+        details: data.details,
+        maximumAttendees: data.maximumAttendees,
+        slug,
+        }
+    })
+
+    return reply.status(201).send({eventId: event.id})
+
+    })  
+}
+
